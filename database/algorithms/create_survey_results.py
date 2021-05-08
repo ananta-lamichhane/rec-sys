4147f680c82564b6645dd4976febc835e1b5c09e
ananta.lamichhane <ananta.lamichhane@campus.tu-berlin.de>
2021-05-08 09:24:21 +0200

    deleted unnecessary files

--- database/algorithms/create_survey_results.py
+++
@@ -1,3 +0,0 @@
-## TODO: create a csv or json file with 10 movies and ratings at random
-## TODO: create a score that says how good the user scored the recommendation (1-10 step 0.5)
-## we'll use these to test the
