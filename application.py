300bbef98ca809c432253210bdddbb2541ed2ca1
ananta.lamichhane <ananta.lamichhane@campus.tu-berlin.de>
2021-05-08 09:42:52 +0200

    cleanup, fixed relative import error

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .views import main_bp
+from views import main_bp
 
 
 login_database = SQLAlchemy() ## initialize a new database
@@ -9,8 +9,7 @@ login_manager= LoginManager()
 
 def create_app(): #config_file='flask_config.py'):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'aklsfjlaksjdi8uiii390oll'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://///tmp/test.db"
+    app.config.from_object('flask_config.Configurations')
     login_database.init_app(app)
     login_manager.init_app(app)
 
