77a664a44022cb183b871ed29b53870bb35517cd
ananta.lamichhane <ananta.lamichhane@campus.tu-berlin.de>
2021-05-08 15:59:48 +0200

    now imports user, database and user_datastore from dbmodel

--- application.py
+++ application.py
@@ -1,20 +1,34 @@
 from flask import Flask
+from flask_security import Security
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from views import main_bp
+import email_validator
+
+
 
 
 login_database = SQLAlchemy() ## initialize a new database
-login_manager= LoginManager()
+login_manager = LoginManager()
+
+
 
 def create_app(): #config_file='flask_config.py'):
     app = Flask(__name__)
     app.config.from_object('flask_config.Configurations')
     login_database.init_app(app)
-    login_manager.init_app(app)
 
-    with app.app_context():
+    from database.user_model import User, Role, user_datastore
+    security = Security(app, user_datastore)
+    with app.app_context(): ### need application context to initialize database.
         login_database.create_all()
+        login_database.session.commit()
+        login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+    from views import main_bp
 
     app.register_blueprint(main_bp)
 
