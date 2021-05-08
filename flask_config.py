1200d89686383c99e55dfc1e2ba87805c554296a
ananta.lamichhane <ananta.lamichhane@campus.tu-berlin.de>
2021-05-08 09:41:28 +0200

    separated flask configurations

--- flask_config.py
+++ flask_config.py
@@ -1,2 +1,9 @@
 import os
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class Configurations:
+    FLASK_APP ='run.py'
+    SECRET_KEY = 'aklkadf8934rklls0f90a9f0' ## generate truly secure random string later.
+    FLASK_ENV = 'Testing'
+    SQLALCHEMY_DATABASE_URI = "sqlite://///tmp/test.db" ## temporary, replace with pgsql db later
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  ##supresses track modification memory usage warning
