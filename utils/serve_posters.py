88e72e75b3e81be4e6d175feda5b88aaa40929e0
ananta-lamichhane <bloomsnruins@gmail.com>
2021-05-24 10:43:27 +0200

    serves dict instead of list

--- utils/serve_posters.py
+++ utils/serve_posters.py
@@ -5,16 +5,16 @@ import random
 
 def generate_poster_url_dict(imdb_ids):
     """
+    generates a list of poster urls
     :param imdb_ids: list of movie ids
     :return: list of urls to corresponding posters.
     """
     client = OMDBClient(apikey=os.environ.get('OMDB_API_KEY'))
-    poster_url_list = []
+    poster_url_list = {}
     for imdb_id in imdb_ids:
         data = client.get(imdbid=imdb_id)
         poster_url = data['poster']
-        print(poster_url)
-        poster_url_list.append(poster_url)
+        poster_url_list[imdb_id] = poster_url
+        #poster_url_list['image_url'] = poster_url
 
     return poster_url_list
-
