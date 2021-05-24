3f7d21562cc7e8565f40e2d0b4c5d3bdaeb0ec1a
ananta-lamichhane <bloomsnruins@gmail.com>
2021-05-24 15:20:51 +0200

    added database models for different tables

--- database/user_model.py
+++ database/user_model.py
@@ -15,21 +15,48 @@ roles_users = login_database.Table('roles_users',
 
 
 class Role(login_database.Model, RoleMixin):
+    __tablename__ = 'role'
     id = login_database.Column(login_database.Integer(), primary_key=True)
     name = login_database.Column(login_database.String(80), unique=True)
     description = login_database.Column(login_database.String(255))
 
 
+class Dataset(login_database.Model):
+    __tablename__ = 'dataset'
+    id = login_database.Column(login_database.Integer(), primary_key=True)
+    name = login_database.Column(login_database.String(200), unique=True)
+    size = login_database.Column(login_database.Integer)
+    category = login_database.Column(login_database.String(200))
+    description = login_database.Column(login_database.String(200))
+
+
 class User(login_database.Model, UserMixin):
-    id = login_database.Column(login_database.Integer, primary_key=True)
+    __tablename__ = 'user'
+    id = login_database.Column(login_database.Integer(), primary_key=True)
     email = login_database.Column(login_database.String(255), unique=True)
     password = login_database.Column(login_database.String(255))
     username = login_database.Column(login_database.String(255))
+    token_id = login_database.Column(login_database.Integer) ## unique?
     active = login_database.Column(login_database.Boolean())
     confirmed_at = login_database.Column(login_database.DateTime())
     ratings = login_database.Column(login_database.String(1024))
     roles = login_database.relationship('Role', secondary=roles_users,
                                         backref=login_database.backref('users', lazy='dynamic'))
+
+
+class Rating(login_database.Model):
+    __tablename__ = 'rating'
+    id = login_database.Column(login_database.Integer(), primary_key=True)
+    rating = login_database.Column(login_database.String(512))
+    dataset_id = login_database.Column(login_database.Integer, login_database.ForeignKey('dataset.id'))
+
+
+class Item(login_database.Model):
+    __tablename__ = 'item'
+    id = login_database.Column(login_database.Integer(), primary_key=True)
+    name = login_database.Column(login_database.String(200))
+    imdb_id= login_database.Column(login_database.Integer(), unique=True)
+    poster_url = login_database.Column(login_database.String(1024))
 
 
 # Setup Flask-Security
