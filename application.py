a4ea7870c925b682dc71dd89b78e30af6f09a173
ananta-lamichhane <bloomsnruins@gmail.com>
2021-05-28 15:57:53 +0200

    adds admin 50 default users - no register required

--- application.py
+++ application.py
@@ -7,7 +7,6 @@ import email_validator
 
 
 
-
 load_dotenv('.ENV')
 login_database = SQLAlchemy() ## initialize a new database
 login_manager = LoginManager() ## login manager helps implement login_required, etc.
@@ -23,16 +22,22 @@ def create_app(): #config_file='flask_config.py'):
 
     app.register_blueprint(main_bp) ## register blueprint
 
-    from database.user_model import User, Role, user_datastore
+    from database.user_model import User
+    from database.user_model import user_datastore
     security = Security(app, user_datastore)
     with app.app_context(): ### need application context to initialize database.
         login_database.create_all()
         login_database.session.commit()
         login_manager.init_app(app)
+        if not login_database.session.query(User).filter_by(name='admin').first(): ## create admin and 50 users for the first time
+            user_datastore.create_user(token_id=9999, name='admin', password='admin', online_user=True)
+            for u in range(2,52):
+                user_datastore.create_user(token_id=u, name='user'+str(u), password='password', online_user=True)
+            login_database.session.commit()
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(user_id)
+        return login_database.session.query(User).filter_by(id=user_id).first()
 
     @login_manager.unauthorized_handler ## serves forbidden.html in case user no logged in.
     def unauth_handler():
