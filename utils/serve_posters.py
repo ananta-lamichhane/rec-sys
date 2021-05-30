135f3b742b5c6ccb5212e218f837ad3734994b5b
ananta-lamichhane <bloomsnruins@gmail.com>
2021-05-30 10:16:35 +0200

    now serves individual poster data

--- utils/serve_posters.py
+++ utils/serve_posters.py
@@ -3,25 +3,29 @@ import os
 import random
 
 
-def generate_poster_url_dict(imdb_ids):
+# def generate_poster_url_dict(imdb_ids):
+#     """
+#     generates a list of poster urls
+#     :param imdb_ids: list of movie ids
+#     :return: list of urls to corresponding posters.
+#     """
+#     client = OMDBClient(apikey=os.environ.get('OMDB_API_KEY'))
+#     poster_url_list = {}
+#     for imdb_id in imdb_ids:
+#         data = client.get(imdbid=imdb_id)
+#         poster_url_list[imdb_id] = data
+#
+#     return poster_url_list
+def generate_poster_url_dict(imdb_id):
     """
     generates a list of poster urls
-    :param imdb_ids: list of movie ids
+    :param imdb_id:
     :return: list of urls to corresponding posters.
     """
     client = OMDBClient(apikey=os.environ.get('OMDB_API_KEY'))
-    poster_url_list = {}
-    for imdb_id in imdb_ids:
-        data = client.get(imdbid=imdb_id)
-        poster_url_list[imdb_id] = data
+    data = client.get(imdbid=imdb_id)
+    return data
 
-    return poster_url_list
-#def generate_poster_url_dict(imdb_id):
- #   """
-  #  generates a list of poster urls
-   # :param imdb_ids: list of movie ids
-    #:return: list of urls to corresponding posters.
-#    """
- #   client = OMDBClient(apikey=os.environ.get('OMDB_API_KEY'))
-  #  data = client.get(imdbid='tt' + str(imdb_id))
-   # return data
+
+def load_next_movie(current_id):
+    return current_id + 1
