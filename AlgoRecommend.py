80d5981a298a7ae94915953fce748ac28a0cb8ed
ananta-lamichhane <bloomsnruins@gmail.com>
2021-06-01 22:17:21 +0200

    now saves results as csv

--- AlgoRecommend.py
+++ AlgoRecommend.py
@@ -10,6 +10,8 @@ from surprise import CoClustering
 from surprise import SVD
 from surprise import Reader
 from surprise import Dataset
+import pandas as pd
+import numpy as np
 
 from collections import defaultdict
 from operator import itemgetter
@@ -24,12 +26,14 @@ def load_dataset():
     ratings_dataset = 0
     
     # specify the columns order to read the data correctly
-    reader= Reader(line_format='user item rating timestamp', sep=',', skip_lines=1) 
-    ratings_dataset= Dataset.load_from_file('ml-latest-small/ratings.csv', reader=reader)
+    reader= Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
+    filepath1 = os.path.realpath('./database/datasets/movielens_small/ratings.csv')
+    ratings_dataset= Dataset.load_from_file(filepath1, reader=reader)
     
     # link movie movie's name with its ID 
     movieID_to_Name={}
-    with open('ml-latest-small/movies.csv', newline='', encoding='ISO-8859-1')as csvfile:
+    filepath = os.path.realpath('./database/datasets/movielens_small/movies.csv')
+    with open(filepath, newline='', encoding='ISO-8859-1')as csvfile:
         movie_reader=csv.reader(csvfile)
         next(movie_reader)
         for row in movie_reader:
@@ -62,54 +66,63 @@ similarity_matrix = KNNBaseline(sim_options=sim_options).fit(trainset).compute_s
 
 # pick a random user ID, has to be a numerical string
 #see how final recommendations change
-# depending on the user 1-610
-test_subject= '500'
+all_recoms = []
+for i in range(1,611):
+    # depending on the user 1-610
+    test_subject= str(i)
 
-#Get thr top K items user rated
-k=20
+    #Get thr top K items user rated
+    k=20
 
-# Convert raw ids (string and integers) to inned ids as a unique integer
-#so Surprise could manipulate them 
-test_subject_iid= trainset.to_inner_uid(test_subject)
-
-
-#get the top K items our user rated (sorted)
-test_subject_ratings = trainset.ur[test_subject_iid]
-k_neighbors = heapq.nlargest(k, test_subject_ratings, key=lambda t: t[1])
+    # Convert raw ids (string and integers) to inned ids as a unique integer
+    #so Surprise could manipulate them
+    test_subject_iid= trainset.to_inner_uid(test_subject)
 
 
-#Defaultdict when trying to access a key that does not exist it creates a new entry with that key, no error
-candidates = defaultdict(float)
-
-for itemID , rating in k_neighbors:
-    try:
-        similarities = similarity_matrix[itemID]
-        for innerID, score in enumerate(similarities):
-            candidates[innerID]+= score * (rating/5.0)
-    except:
-        continue
+    #get the top K items our user rated (sorted)
+    test_subject_ratings = trainset.ur[test_subject_iid]
+    k_neighbors = heapq.nlargest(k, test_subject_ratings, key=lambda t: t[1])
 
 
-# Build a dictionary of movies the user has watched
-# trainset.ur[test_subject_iid] are items rated by the specific user
-watched={}
-for itemID, rating in trainset.ur[test_subject_iid]:
-    watched[itemID]= 1
+    #Defaultdict when trying to access a key that does not exist it creates a new entry with that key, no error
+    candidates = defaultdict(float)
 
-# add not watched movies and similar to his favorite movies to recommendation list
-recommendations =[]
+    for itemID , rating in k_neighbors:
+        try:
+            similarities = similarity_matrix[itemID]
+            for innerID, score in enumerate(similarities):
+                candidates[innerID]+= score * (rating/5.0)
+        except:
+            continue
 
-position = 0
-for itemID, rating_sum in sorted(candidates.items(), key=itemgetter(1), reverse=True):
-    if not itemID in watched:
-        recommendations.append(getMovieName(trainset.to_raw_iid(itemID)))
-        position +=1
-        if(position >10): break # only top 10 movies to recommend
 
-for rec in recommendations:
-    print("Movie:", rec)
+    # Build a dictionary of movies the user has watched
+    # trainset.ur[test_subject_iid] are items rated by the specific user
+    watched={}
+    for itemID, rating in trainset.ur[test_subject_iid]:
+        watched[itemID]= 1
+
+    # add not watched movies and similar to his favorite movies to recommendation list
+    recommendations =[]
+
+    position = 0
+    for itemID, rating_sum in sorted(candidates.items(), key=itemgetter(1), reverse=True):
+        if not itemID in watched:
+            #recommendations.append(itemID)
+            real_moviid= trainset.to_raw_iid(itemID)
+            recommendations.append(real_moviid)
+            #recommendations.append(getMovieName(trainset.to_raw_iid(itemID)))
+            position +=1
+            if(position ==10): break # only top 10 movies to recommend
+    all_recoms.append(recommendations)
+
+    for rec in recommendations:
+        print("Movie:", rec)
     
-    
+save_path=os.path.realpath('./database/datasets/movielens_small/recom_knn_ds1.csv')
+recsdf = pd.DataFrame(all_recoms)
+recsdf.index = np.arange(1, len(recsdf)+1)
+recsdf.to_csv(save_path, sep=",")
     
     
     
